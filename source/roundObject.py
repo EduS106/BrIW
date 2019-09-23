c76f0dcc91eef8a8e9c3e59ea48266c0d6593b56
Eduardo Hernan Salazar Caldentey <eduardo.salazar-caldentey@infinityworks.com>
2019-09-23 15:42:33 +0100

    updating roundObject for implementation

--- source/roundObject.py
+++ source/roundObject.py
@@ -1,20 +1,38 @@
+from source.app import number_cleaner, name_cleaner
 class Round:
     active = True
-
-    def __init__(self, brewer, initial_orders={}):
+    orders = {}
+    # team must be stored in a teams file/database
+    def __init__(self, brewer, preferences={}, team=[]):
         self.brewer = brewer
-        self.orders = initial_orders
+        self.preferences = preferences
+        self.team = team
+        self.opening_message = f"Welcome, {brewer}, you have started a new round!"
+
+    def welcome(self):
+        print(self.opening_message)
 
     def add_order(self, person, drink):
         self.orders.update({person: drink})
 
-    def remove_order(self, person):
+    def _remove_order(self, person):
         self.orders.pop(person)
 
-    def clear(self):
+    def _clear_orders(self):
         self.orders.clear()
 
-    def finish_round(self):
+    def close_round(self):
         self.active = False
 
-newRound = Round("Eduardo")
+def start_rounds():
+    brewer = input("What is your name?\nName: ")
+    newRound = Round(brewer)
+    newRound.welcome()
+    menu = f'''What would you like to do? Choose from the following options:
+    
+[1] Add order
+[2] Add team mate
+[3] Add '''
+
+
+start_rounds()
